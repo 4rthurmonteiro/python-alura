b354cfee1d00e30e04eacb3488b183e638be968c
Arthur Monteiro <monteiroamelo@gmail.com>
2021-04-10 17:14:08 -0300

    curso 1 feito

--- python3-part1/jogos/adivinhacao.py
+++ python3-part1/jogos/adivinhacao.py
@@ -1,1 +1,30 @@
-print("Bem vindo ao jogo de Adivinhação!")
+print('*********************************')
+print("Bem vindo no jogo de Adivinhação!")
+print('*********************************')
+
+numero_secreto = 42
+total_de_tentativas = 3
+
+while total_de_tentativas > 0:
+    print("Tentativa: ", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+
+    print("Vc digitou ", chute_str)
+
+    chute = int(chute_str)
+
+    acertou = numero_secreto == chute
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if acertou:
+        print("Vc acertou!")
+        break
+    else:
+        if maior:
+            print("Vc errou! Chute foi maior")
+        elif menor:
+            print("Vc errou! Chute foi menor")
+
+    total_de_tentativas = total_de_tentativas - 1
+print("Fim do jogo")
